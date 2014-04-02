e4fdfe5c9f179f46b42c437d21d2b406fb5b475c
DeanIsseyegh <issdea@gmail.com>
2014-04-02 21:02:42 +0100

    Added first neo4j query!2

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,22 @@
  */
 
 app = require('../app');
+var neoReq = require('superagent');
 
 app.get('/', function(req, res){
-	res.send('home!');
+	res.send('homeUpdated!');
 })
 
 app.post("/", function(req, res){
 	console.log('POST /');
 	console.dir(req.body);
+	consosle.log(process.env.NEO4J_REST_URL);
+	neoReq.post(process.env.NEO4J_REST_URL + '/cypher').send({
+		query: 'CREATE (n {name:"World"}) RETURN "hello", n.name'
+	}).end(function(neo4jRes)) {
+		res.send(neo4jRes.text);
+	}
+
 	res.writeHead(200, {'Content-type': 'application/json'});
 	//res.end("thanks");
 })
