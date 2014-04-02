5f17d3ae106dca81366a0794d4dd11f0bccc3df8
DeanIsseyegh <issdea@gmail.com>
2014-04-02 22:04:53 +0100

    4

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@
  */
 app = require('../app');
 var neoReq = require('superagent');
+var db = process.env.GRAPHENEDB_URL
 
 app.get('/', function(req, res){
 	res.send('homeUpdated!');
@@ -12,12 +13,23 @@ app.get('/', function(req, res){
 app.post("/", function(req, res){
 	console.log('POST /');
 	console.dir(req.body);
-	console.dir(process.env.GRAPHENEDB_URL);
-	neoReq.post(process.env.GRAPHENEDB_URL + '/cypher').send({
-		query: 'CREATE (n {name:"World"}) RETURN "hello", n.name'
+
+	db.insertNode({
+		name: 'Darth Vader',
+		sex:  'male'
+	}, function(err, node){
+		if(err) throw err;
+		console.log(node.data);
+		console.log(node.id);
 	}).end(function(neo4jRes) {
 		res.send(neo4jRes.text);
-	});
+	}
+
+	//neoReq.post(process.env.GRAPHENEDB_URL + '/cypher').send({
+	//	query: 'CREATE (n {name:"World"}) RETURN "hello", n.name'
+	//}).end(function(neo4jRes) {
+	//	res.send(neo4jRes.text);
+	//});
 
 	//res.writeHead(200, {'Content-type': 'application/json'});
 	//res.end("thanks");
