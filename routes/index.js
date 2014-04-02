8dd86f05dcc91210c5ec48b7957a2e1bafaa470b
DeanIsseyegh <issdea@gmail.com>
2014-04-02 22:23:33 +0100

    99

--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,7 @@ app.post("/", function(req, res){
 	console.log('POST /');
 	console.dir(req.body);
 
-	db.insertNode(
-	{
-		name: 'Darth Vader',
-		sex:  'male'
-	}}
-	));
+	);
 
 	//neoReq.post(process.env.GRAPHENEDB_URL + '/cypher').send({
 	//	query: 'CREATE (n {name:"World"}) RETURN "hello", n.name'
